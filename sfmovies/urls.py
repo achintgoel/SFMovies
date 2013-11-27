92d70c38ee411712bef713954d9e4eb05df63870
agoel <achintg12@gmail.com>
2013-11-27 12:45:00 -0800

    changed url to point directly to home

--- sfmovies/urls.py
+++ sfmovies/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import RedirectView
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sfmovies.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', RedirectView.as_view(url='/datasf/home')),
     url(r'^datasf/', include('datasf.urls', namespace="datasf")),
     url(r'^admin/', include(admin.site.urls)),
 )
